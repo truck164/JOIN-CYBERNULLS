3d92f8d2f92ff84a8fcad354041e3e9f89898d35
T4rm1nal-L1nux <68395530+truck164@users.noreply.github.com>
2021-07-26 21:51:09 +0800

    How to install
    .

---
+++ (WANT TO JOIN?).py
@@ -0,0 +1,34 @@
+#SIMPLE AI BY CYBER ANT
+
+
+print("If you want to join to CyberNulls team")
+print("We need some information about you")
+
+print("Is that ok?")
+say = input("[!] (y/n) ")
+print("Ok if '" + say + "' Then ok")
+
+print("[!] HELLO WHAT IS YOUR NAME? [!]")
+talk = input("[!] ")
+
+print("[!] OH HELLO " + talk + " NICE TO MEET YOU IAM CYBER ANT I AM A IA HOW OLD ARE YOU? [!] " )
+
+age = input("[!]")
+print("[!] OH SO YOUR AGE IS '" + age + "' [!]")
+
+print("[!] YOU WANT TO JOIN CYBERNULLS TEAM? [!]")
+talk2 = input("[!] ")
+
+print("[!] YOU MEAN " + talk2 + " YEAH SURE ITS FREE TO JOIN")
+print()
+print()
+
+print("[!] PROGRAMING LANGUAGE YOU KNOW? [!]")
+talk3 = input("[!] ")
+
+print("OH THATS IT ONLY  " + " ?")
+print("ITS OK")
+print("YOUR NOW JOINED TO")
+print("!")
+print("--CYBERNULLS--")
+print("     TEAM     ")
